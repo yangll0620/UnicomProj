8d239390915b5e7805d7a4164ec111ef2c8fa90f
yangll0620 <yll0620@gmail.com>
2018-09-27 15:27:13 +0800

    add __main__ part

--- Complexity/prediction_ANN.py
+++ Complexity/prediction_ANN.py
@@ -2,7 +2,7 @@
 # @Author: yll
 # @Date:   2018-09-13 11:11:04
 # @Last Modified by:   yangll0620
-# @Last Modified time: 2018-09-27 10:36:14
+# @Last Modified time: 2018-09-27 15:23:13
 
 import time
 import csv
@@ -36,37 +36,40 @@ def prediction_perpair(file_train, file_test):
 	del x_train, y_train, x_test, y_test
 	return precision, recall, TNR, F_measure, G_mean, time_train
 
-path_Dropbox = '/home/yll/Dropbox/'
-path_Proj = path_Dropbox + 'workSpace/SYSU/Projects/UnicomProject/Manuscripts/JournalJason/Complexity/'
-folder_load = path_Proj + 'Lidongyang/OriginalData/'
-file_07 =  folder_load + 'ZDJM_4G_XQ12_201507_T2_YUAN.csv'
-file_08 =  folder_load + 'ZDJM_4G_XQ12_201508_T2_YUAN.csv'
-file_09 =  folder_load + 'ZDJM_4G_XQ12_201509_T2_YUAN.csv'
-file_10 =  folder_load + 'ZDJM_4G_XQ12_201510_T2_YUAN.csv'
+def prediction_ANN():
+	path_Dropbox = '/home/yll/Dropbox/'
+	path_Proj = path_Dropbox + 'workSpace/SYSU/Projects/UnicomProject/Manuscripts/JournalJason/Complexity/'
+	folder_load = path_Proj + 'Lidongyang/OriginalData/'
+	file_07 =  folder_load + 'ZDJM_4G_XQ12_201507_T2_YUAN.csv'
+	file_08 =  folder_load + 'ZDJM_4G_XQ12_201508_T2_YUAN.csv'
+	file_09 =  folder_load + 'ZDJM_4G_XQ12_201509_T2_YUAN.csv'
+	file_10 =  folder_load + 'ZDJM_4G_XQ12_201510_T2_YUAN.csv'
 
-path_save = path_Proj + 'Major Revision/Results/'
-savefile_name = 'results_ANN.csv'
-with open(path_save+savefile_name,'w') as csv_file:
-	fieldnames = ['type','precision','recall','TNR','F-measure','G-mean','train time']
-	writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
-	writer.writeheader() # write the header
+	path_save = path_Proj + 'Major Revision/Results/'
+	savefile_name = 'results_ANN.csv'
+	with open(path_save+savefile_name,'w') as csv_file:
+		fieldnames = ['type','precision','recall','TNR','F-measure','G-mean','train time']
+		writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
+		writer.writeheader() # write the header
 
-	# 7 to 8
-	[precision, recall, TNR, F_measure, G_mean, time_train] = prediction_perpair(file_07, file_08)
-	writer.writerow({'type':'7 to 8','precision':precision,'recall':recall
-		,'TNR':TNR,'F-measure':F_measure,'G-mean':G_mean,'train time':time_train})
-	del precision, recall, TNR, F_measure, G_mean, time_train
+		# 7 to 8
+		[precision, recall, TNR, F_measure, G_mean, time_train] = prediction_perpair(file_07, file_08)
+		writer.writerow({'type':'7 to 8','precision':precision,'recall':recall
+			,'TNR':TNR,'F-measure':F_measure,'G-mean':G_mean,'train time':time_train})
+		del precision, recall, TNR, F_measure, G_mean, time_train
 
-	# 8 to 9
-	[precision, recall, TNR, F_measure, G_mean, time_train] = prediction_perpair(file_08, file_09)
-	writer.writerow({'type':'8 to 9','precision':precision,'recall':recall
-		,'TNR':TNR,'F-measure':F_measure,'G-mean':G_mean,'train time':time_train})
-	del precision, recall, TNR, F_measure, G_mean, time_train
+		# 8 to 9
+		[precision, recall, TNR, F_measure, G_mean, time_train] = prediction_perpair(file_08, file_09)
+		writer.writerow({'type':'8 to 9','precision':precision,'recall':recall
+			,'TNR':TNR,'F-measure':F_measure,'G-mean':G_mean,'train time':time_train})
+		del precision, recall, TNR, F_measure, G_mean, time_train
 
-	# 9 to 10
-	[precision, recall, TNR, F_measure, G_mean, time_train] = prediction_perpair(file_09, file_10)
-	writer.writerow({'type':'9 to 10','precision':precision,'recall':recall
-		,'TNR':TNR,'F-measure':F_measure,'G-mean':G_mean,'train time':time_train})
-	del precision, recall, TNR, F_measure, G_mean, time_train
+		# 9 to 10
+		[precision, recall, TNR, F_measure, G_mean, time_train] = prediction_perpair(file_09, file_10)
+		writer.writerow({'type':'9 to 10','precision':precision,'recall':recall
+			,'TNR':TNR,'F-measure':F_measure,'G-mean':G_mean,'train time':time_train})
+		del precision, recall, TNR, F_measure, G_mean, time_train
 
 
+if __name__ == "__main__":
+	prediction_ANN()
