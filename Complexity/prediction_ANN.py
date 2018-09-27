8b7fe0323a8f39416aca036138bca13c9803bc3a
yangll0620 <yll0620@gmail.com>
2018-09-27 10:42:05 +0800

    debug the different save folder problem

--- Complexity/prediction_ANN.py
+++ Complexity/prediction_ANN.py
@@ -2,7 +2,7 @@
 # @Author: yll
 # @Date:   2018-09-13 11:11:04
 # @Last Modified by:   yangll0620
-# @Last Modified time: 2018-09-25 19:25:10
+# @Last Modified time: 2018-09-27 10:36:14
 
 import time
 import csv
@@ -36,16 +36,17 @@ def prediction_perpair(file_train, file_test):
 	del x_train, y_train, x_test, y_test
 	return precision, recall, TNR, F_measure, G_mean, time_train
 
-path_Dropbox = '~/Dropbox/workSpace/SYSU/Projects/UnicomProject/Manuscripts/JournalJason/Complexity/'
-folder_load = path_Dropbox + 'Lidongyang/OriginalData/'
+path_Dropbox = '/home/yll/Dropbox/'
+path_Proj = path_Dropbox + 'workSpace/SYSU/Projects/UnicomProject/Manuscripts/JournalJason/Complexity/'
+folder_load = path_Proj + 'Lidongyang/OriginalData/'
 file_07 =  folder_load + 'ZDJM_4G_XQ12_201507_T2_YUAN.csv'
 file_08 =  folder_load + 'ZDJM_4G_XQ12_201508_T2_YUAN.csv'
 file_09 =  folder_load + 'ZDJM_4G_XQ12_201509_T2_YUAN.csv'
 file_10 =  folder_load + 'ZDJM_4G_XQ12_201510_T2_YUAN.csv'
 
-path_save = path_Dropbox + 'Major Revision/Results/'
-savefile_name = path_save + 'results_ANN.csv'
-with open(savefile_name,'w') as csv_file:
+path_save = path_Proj + 'Major Revision/Results/'
+savefile_name = 'results_ANN.csv'
+with open(path_save+savefile_name,'w') as csv_file:
 	fieldnames = ['type','precision','recall','TNR','F-measure','G-mean','train time']
 	writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
 	writer.writeheader() # write the header
